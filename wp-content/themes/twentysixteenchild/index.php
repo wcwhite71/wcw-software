c8deccf9869bf753e38ffab9b96db09a689fc35e
William White <william.white@nthrive.com>
2019-10-11 16:05:51 -0600

    customized

--- wp-content/themes/twentysixteenchild/index.php
+++ wp-content/themes/twentysixteenchild/index.php
@@ -3,9 +3,24 @@
 	<head>
 		<meta charset="utf-8">
 		<title>TEST PAGE</title>
-		<script src="../../../../bootstrap-4.3.1/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="./jquery/jquery.min.js"></script>
+		<script type="text/javascript" src="./bootstrap4.3.1/js/bootstrap.min.js"></script>
 	</head>
-	<body>
-		<h1>IT WORKS!</h1>
-	</body>
+
+	<?php
+			get_header()
+	?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+					<header>
+						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					</header>
+
+		</main>
+	</div>
+
+	<?php get_sidebar(); ?>
+	<?php get_footer(); ?>
+
 </html>
